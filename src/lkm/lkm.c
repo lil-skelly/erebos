11303eda1ec3000d19c058d42064c5d2ba30ebec
skelly <skelly@arch.org>
2024-09-01 20:29:08 +0300

    Added a basic LKM to use with the loader

---
+++ src/lkm/lkm.c
@@ -0,0 +1,21 @@
+/* Bare-bones LKM (Loaded Kernel Module) to use with the loader */
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("lil-skelly");
+MODULE_DESCRIPTION("Basic LKM for the Erebos loader");
+MODULE_VERSION("1.0");
+
+static int __init lkm_init(void) {
+  pr_info("lkm: Loaded sample LKM.\n");
+  return 0;
+}
+
+static void __exit lkm_exit(void) {
+    pr_info("lkm: Unloaded sample LKM.\n");
+}
+
+module_init(lkm_init);
+module_exit(lkm_exit);
