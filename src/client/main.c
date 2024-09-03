49ab3ddc4b855a00559defd796985b05cb3ecf7f
ekardnamm <lucabertozzi.pub@gmail.com>
2024-09-03 21:57:39 +0800

    Added simple HTTP GET functionality

--- src/client/main.c
+++ src/client/main.c
@@ -1,25 +1,18 @@
 #include <sys/types.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-#include "sock.h"
+#include "http.h"
 /* Networking constants */
 #define SERVER_IP "127.0.0.1"
 #define SERVER_PORT "8000"
 
 int main() {
-  struct addrinfo hints, *res;
-  int sfd; // socket file descriptor
-
-  setup_hints(&hints);
-
-  if (h_getaddrinfo(SERVER_IP, SERVER_PORT, &hints, &res) != 0) {
-    return EXIT_FAILURE;
-  }
-  sfd = create_sock_and_conn(res);
-  if (sfd == -1) {
-    return EXIT_FAILURE;
+  http_res_t response;
+  if (HTTP_SUCCESS != http_get(SERVER_IP, SERVER_PORT, "/hello", &response)) {
+    return EXIT_FAILURE;  
   }
 
-  request_download(sfd);
+  write(1, response.data, response.size);
   return EXIT_SUCCESS;
 }
